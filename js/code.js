3f1325353cb0be05e1e3b8e9fff56aa1e6e45cb3
mcaland <maxwellcalandro@gmail.com>
2025-02-13 14:33:12 -0500

    test js for adding contact and search

--- js/code.js
+++ js/code.js
@@ -78,3 +78,62 @@ function register() {
         document.getElementById("error-message").innerHTML = error.message;
     }
 }
+
+function search()
+{
+    let searchRequest = { userID: id, search: document.getElementById("searchInput").value };
+    let jsonPayload = JSON.stringify(searchRequest);
+
+    let URL = baseURL + "/SearchContact." + extension;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", URL, true);
+    request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let jsonObj = JSON.parse(request.responseText);
+                document.getElementById("contactsTableBody").innerHTML = "";
+
+                for (let i = 0; i < jsonObj.length; i++)
+                {
+                    document.getElementById("contactsTableBody").innerHTML += "<tr id=\"" + jsonObj[i].split(" ")[0] + "\"> <td>" + jsonObj[i].split(" ")[1] + "</td> <td>" + jsonObj[i].split(" ")[2] + "</td> <td>" + jsonObj[i].split(" ")[3] + "</td> <td> Actions go here... <td>";
+                }
+            }
+        };
+        request.send(jsonPayload);
+    }
+    catch (error) {
+        document.getElementById("error-message").innerHTML = error.message;
+    }
+}
+
+function add_contact()
+{
+    let addRequest = { userID: id, name: document.getElementById("editName").value, email: document.getElementById("editEmail").value, phoneNum: document.getElementById("editPhone").value };
+    let jsonPayload = JSON.stringify(addRequest);
+
+    let URL = baseURL + "/AddContact." + extension;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", URL, true);
+    request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let jsonObj = JSON.parse(request.responseText);
+                document.getElementById("contactsTableBody").innerHTML = "";
+
+                // TODO: error on modal if it doesn't add
+
+                search(); // update list just in case
+                return;
+            }
+        };
+        request.send(jsonPayload);
+    }
+    catch (error) {
+        document.getElementById("error-message").innerHTML = error.message;
+    }
+}
+
