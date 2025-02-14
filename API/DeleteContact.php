c6e718e20a1734440a6700c81fa15682fe60dc65
Alex <canappa@wwu.edu>
2025-02-13 23:16:12 -0500

    made sure to close database connection no matter outcome

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -6,39 +6,40 @@ $inData = getRequestInfo();
 $contactID = $inData["contactID"];
 
 // Connect to database and checking connection
-$conn = new mysqli("localhost", "admin","Password!", "SmallProj");
+$conn = new mysqli("localhost", "admin", "Password!", "SmallProj");
 
 if ($conn->connect_error) {
-    returnWithError($conn->connect_error); 
-} else{
+    returnWithError($conn->connect_error);
+} else {
     // Make the querry DELETE the contact by using the contactID
     $stmt = $conn->prepare("DELETE FROM Contacts WHERE contactID = ?");
     $stmt->bind_param("i", $contactID);
     $stmt->execute();
 
     // Check to see if one or more rows were actually deleted
-    if($stmt->affected_rows > 0){
-        $stmt->close();
-        $conn->close();
+    if ($stmt->affected_rows > 0) {
         returnWithError("");
-    }else{ // If not, there was no contact with contactID
+    } else { // If not, there was no contact with contactID
         returnWithError("No contact found with that contactID.");
     }
+    $stmt->close();
+    $conn->close();
 }
- 
+
 // FUNCTIONS
-function getRequestInfo(){
-	return json_decode(file_get_contents('php://input'), true);
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
 }
 
-function sendResultInfoAsJson($obj){
+function sendResultInfoAsJson($obj)
+{
     header('Content-type: application/json');
-	echo $obj;
+    echo $obj;
 }
 
-function returnWithError($err){
+function returnWithError($err)
+{
     $retValue = '{"error":"' . $err . '"}';
     sendResultInfoAsJson($retValue);
 }
-
-?>
