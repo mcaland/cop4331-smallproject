213ef3a20d5511dd683d6c4ff1f358ce880af5f6
Andrew <andrewbulatao77@gmail.com>
2025-02-17 18:12:42 -0500

    Enabled CORs to DeleteContact to see if it fixes the error with swaggerhub

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 // Retrieve data
 $inData = getRequestInfo();
 // Just need to retrive the contactID for deleteing a contact
